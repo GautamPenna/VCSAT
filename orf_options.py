aa61d52f877b032fb1284d09d7d528e6f9291f62
GautamPenna <gautam.penna@gmail.com>
2025-01-09 11:11:53 -0600

    Delete orf_options.py

--- orf_options.py
+++
@@ -1,9 +0,0 @@
-import functions as func
-
-votes_file = input('What do you want the votes file to be called with pathname: ')
-orf_file = input('What is the pathway for the orf file you want: ')
-genotype = input('What is the genotype of this analysis: ')
-
-func.orf_options(votes_file, orf_file, genotype)
-
-print('your file has been created')
