91afb43bd6f4249951b6882f07ecdbe290b7cdb3
GautamPenna <gautam.penna@gmail.com>
2025-01-09 11:11:05 -0600

    Delete creating_consensus_determination_input_files.py

--- creating_consensus_determination_input_files.py
+++
@@ -1,9 +0,0 @@
-import functions as func
-
-input_file = input('What text file from code 3 do you want to make a consensus determination file from? Provide the entire pathway: ')
-output_file = input('What is the pathway for the output_file: ')
-
-func.consensus_determination_file_creator(input_file, output_file)
-
-print('your files have been created in their requested directory')
-
